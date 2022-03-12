474dd6cd4367783395b078f0879f5e3c4f8d1a3b
Xu Desheng <8742037+xudesheng@users.noreply.github.com>
2022-03-12 01:24:57 -0500

    test workflow

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# Changelog
+
+All notable changes to this project will be documented in this file from the release of v4.0.0.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
+
+## [Unreleased]
+
+## [v0.0.1] - 2022-03-09
+
+### Added
+ - test
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let body = reqwest::get("https://www.rust-lang.org")
+        .await?
+        .text()
+        .await?;
+
+    println!("body = {:?}", body);
+    Ok(())
 }
